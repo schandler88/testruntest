5bdf7add47f4c2faebe4a48523cc7353c0787d96
Stephanie Chandler Schober <sschober@cartodb.com>
2020-09-14 15:31:49 -0400

    test

---
+++ src/data/layers/index.js
@@ -0,0 +1,9 @@
+import railaccidents from './railaccidents';
+import usstates from './usstates';
+
+// import neighbourhoods from './neighbourhoods';
+
+// Export order will be the layer order
+export default {
+  railaccidents
+}
